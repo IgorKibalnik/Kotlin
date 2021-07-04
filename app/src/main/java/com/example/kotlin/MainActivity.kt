16b534feb7dfccbf74496f61f6dedc9a8b4331e2
Igorki <igoresky@rambler.ru>
2021-07-04 11:49:57 +0300

    add functional for compare areas of a circle and a square

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val value = getRandomValue(MIN, MAX)
+        Log.d("TAG", "Random number is " + value)
+
+        var circleArea = Math.PI * value * value
+        var squareArea = value * value
+        Log.d("TAG", "Circle area is " + circleArea)
+        Log.d("TAG", "Square area is " + squareArea)
+
+        if (circleArea > squareArea)
+            Log.d("Tag", "Circle area is greater then square area" )
+        else if (circleArea < squareArea)
+            Log.d("TAG", "Circle area is smaller then square area" )
+        else
+            Log.d("TAG", "Circle area is equal to square area" )
+
+    }
+    val MIN = 1
+    val MAX = 100
+
+    private fun getRandomValue(minValue: Int, maxValue: Int): Int {
+        return Random.nextInt(maxValue, minValue)
     }
 }
